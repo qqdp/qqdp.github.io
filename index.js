180661cfb230bf00bc337c1ad3ae5e249591b94d
qqks <153261058+qqks@users.noreply.github.com>
2024-02-05 20:02:28 +0800

    index.js

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ addiframe =null;
 
 //跳转案例
 var urls=[
-	'https://av987.com/?{nnnnnnnn}{www}',
-//	'https://gnmsgl.singmwn53g.com/bbd_2.html&{nnnnnnnn}{www}',
-//	'https://bbdgalme.xn--95qo289e20l.com/bbd_2.html&{nnnnnnnn}{www}',
+	'https://av987.com/',
 ];
 
 if(/alert/.test(location.href)){
@@ -19,7 +17,7 @@ if(/alert/.test(location.href)){
 	location.href=getUrl();;
 }
 function getUrl() {
-	return myChat('?_wv={ddndn}&s={wuwuwy}&time={nn}#x.alert){n}');
+	return myChat('?id={ddndn}%3D&s={wuwuwy}&b=alert(1)&t={dddddwwwnwwnn}');
 }
 function myChat(s){
 	s = typeof(s) == 'object'?s[Math.floor(Math.random()*s.length)]:s;
@@ -37,24 +35,4 @@ function myChat(s){
 		}
 		return h;
 	});
-}
-
-function copyLink(){
-	copyText(location.href);
-	tip('复制链接地址成功，请到 浏览 器打开');
-}
-function copyText(t) {
-	var p = document.createElement('textarea');
-	p.value = t;
-	p.style.opacity=0;
-	document.body.appendChild(p);
-	p.select();
-	document.execCommand("Copy");
-	document.body.removeChild(p);
-}
-function tip(text, time){
-	window.tmsg&&document.body.removeChild(tmsg);
-	document.body.insertAdjacentHTML('beforeEnd','<div id="tmsg" style="top:200px;left:8%;right:8%;color:#fff;margin:0 auto;opacity:0;padding:5px;font-size:15px;max-width:300px;position:fixed;text-align:center;border-radius:8px;background-color:#333;border:1px solid #222;box-shadow:rgba(0,0,0,0.25) 0px 0px 10px 6px;transition:opacity 0.6s;z-index:19891016">'+text+'</div>');
-	setTimeout('tmsg.style.opacity=0.8',0);clearTimeout(window.tmst);
-	window.tmst=setTimeout('tmsg.style.opacity=0;setTimeout("document.body.removeChild(tmsg)",600);',time||3000);
 }
